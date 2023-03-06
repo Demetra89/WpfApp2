e56d05064fa9a9e589a87c3f8a2bfc3b9ffbe373
student203 <student203@203-04>
2023-03-06 10:33:14 +0500

    Добавьте файлы проекта.

---
+++ WpfApp2/Sponsor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+
+    class Sponsor
+    {
+        public string Logo { get; set; }
+        public string Title { get; set; }
+        public int Sum { get; set; }
+    }
+}
